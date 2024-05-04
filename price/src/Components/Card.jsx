29ba5d40f13935455066c927cb0936040298faf7
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-05-04 21:06:07 +0530

    done

--- price/src/Components/Card.jsx
+++ price/src/Components/Card.jsx
@@ -2,35 +2,35 @@ import React from "react";
 
 export default function Card() {
   return (
-    <section className="bg-red-300 flex flex-col justify-center items-center ">
-      <div className="bg-blue-500  h-[50%] sm:w-[40rem] w-[20rem] p-[2.5rem]">
-        <h1>Join our community</h1>
+    <section className=" flex flex-col justify-center items-center  rounded-[0.5rem] sha m-[0.5rem]">
+      <div className="bg-[#ffffff]  h-[50%] sm:w-[40rem] w-[20rem] p-[2.5rem] rounded-t-[0.5rem] "> 
+        <h1 className="text-[#3caba9] font-bold text-[1.5rem] mb-[1rem]">Join our community</h1>
 
-        <p className="">30-day, hassle-free money back guarantee</p>
+        <p className="text-[#bfdf32] font-bold mb-[0.5rem]">30-day, hassle-free money back guarantee</p>
 
-        <p>
+        <p className="text-[#838385] ">
           Gain access to our full library of tutorials along with expert code
           reviews. Perfect for any developers who are serious about honing their
           skills.
         </p>
       </div>
       <div className="flex flex-row justify-center items-center flex-wrap md:flex-nowrap h-[50%]">
-        <div className="bg-red-500 w-[20rem] h-[15.5rem] p-[2.5rem]">
-          <h1>Monthly Subscription</h1>
+      <div className="bg-[#2bb3b1] w-[20rem] h-[15.5rem] p-[2.5rem] text-[#ffffff] sm:rounded-bl-[0.5rem]">
+          <h1 className="mb-[1rem] text-[1.2rem] font-semibold">Monthly Subscription</h1>
 
-          <div className="flex flex-row justify-start items-start gap-[1rem]">
-            &dollar;29 <p className="text-green-300">per month</p>
+          <div className="flex flex-row justify-start items-center gap-[1rem]">
+          <p className="font-bold text-[1.5rem]">$29</p> <p className="text-[#7bdfe0]">per month</p>
           </div>
 
-          <p>Full access for less than &dollar; 1 a day</p>
+          <p className="mb-[1.3rem]">Full access for less than $1 a day</p>
 
-          <button>Sign Up</button>
+          <button className="bg-[#bfdf32] w-full cursor-pointer rounded-[0.5rem] h-[2.7rem] text-center font-bold">Sign Up</button>
         </div>
        
-      <div className="bg-green-500 w-[20rem] h-full p-[2.5rem]">
-        <h1>Why Us</h1>
+      <div className="bg-[#4abebd] w-[20rem] h-[15.5rem] p-[2.5rem] text-[#ffffff] rounded-br-[0.5rem]">
+        <h1 className="font-bold mb-[1rem]">Why Us</h1>
 
-        <p>
+        <p className="text-[#aff5f5] text-[1rem]">
           Tutorials by industry experts Peer &amp; expert code review Coding
           exercises Access to our GitHub repos Community forum Flashcard decks
           New videos every week
