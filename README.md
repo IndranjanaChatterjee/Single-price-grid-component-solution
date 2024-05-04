b69972486203bbdf37991c5eb924e64bf1ab95d3
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-05-04 21:11:17 +0530

    Necessary changes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# Single-price-grid-component-solution
+# Frontend Mentor - Single price grid component solution
+
+This is a solution to the [Single price grid component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/single-price-grid-component-5ce41129d0ff452fec5abbbc). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+### The challenge
+
+Users should be able to:
+
+
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./price/src/assets/screenshot.png)
+
+
+
+
+
+
+
+### Links
+
+- Solution URL: [solution URL](https://github.com/IndranjanaChatterjee/Ping-single-column-coming-soon-page)
+- Live Site URL: [live site URL](https://ping-single-column-coming-soon-page-ten-iota.vercel.app/)
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+- [Tailwind CSS](https://tailwindcss.com/)-CSS Framework# Frontend Mentor - Social links profile solution
+
--- price/src/Components/Card.jsx
+++ price/src/Components/Card.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function Card() {
   return (
     <section className=" flex flex-col justify-center items-center  rounded-[0.5rem] sha m-[0.5rem]">
-      <div className="bg-[#ffffff]  h-[50%] sm:w-[40rem] w-[20rem] p-[2.5rem] rounded-t-[0.5rem] "> 
-        <h1 className="text-[#3caba9] font-bold text-[1.5rem] mb-[1rem]">Join our community</h1>
+      <div className="bg-[#ffffff]  h-[50%] sm:w-[40rem] w-[20rem] p-[2.5rem] rounded-t-[0.5rem] ">
+        <h1 className="text-[#3caba9] font-bold text-[1.5rem] mb-[1rem]">
+          Join our community
+        </h1>
 
-        <p className="text-[#bfdf32] font-bold mb-[0.5rem]">30-day, hassle-free money back guarantee</p>
+        <p className="text-[#bfdf32] font-bold mb-[0.5rem]">
+          30-day, hassle-free money back guarantee
+        </p>
 
         <p className="text-[#838385] ">
           Gain access to our full library of tutorials along with expert code
@@ -15,27 +19,32 @@ export default function Card() {
         </p>
       </div>
       <div className="flex flex-row justify-center items-center flex-wrap md:flex-nowrap h-[50%]">
-      <div className="bg-[#2bb3b1] w-[20rem] h-[15.5rem] p-[2.5rem] text-[#ffffff] sm:rounded-bl-[0.5rem]">
-          <h1 className="mb-[1rem] text-[1.2rem] font-semibold">Monthly Subscription</h1>
+        <div className="bg-[#2bb3b1] w-[20rem] h-[15.5rem] p-[2.5rem] text-[#ffffff] sm:rounded-bl-[0.5rem]">
+          <h1 className="mb-[1rem] text-[1.2rem] font-semibold">
+            Monthly Subscription
+          </h1>
 
           <div className="flex flex-row justify-start items-center gap-[1rem]">
-          <p className="font-bold text-[1.5rem]">$29</p> <p className="text-[#7bdfe0]">per month</p>
+            <p className="font-bold text-[1.5rem]">$29</p>{" "}
+            <p className="text-[#7bdfe0]">per month</p>
           </div>
 
           <p className="mb-[1.3rem]">Full access for less than $1 a day</p>
 
-          <button className="bg-[#bfdf32] w-full cursor-pointer rounded-[0.5rem] h-[2.7rem] text-center font-bold">Sign Up</button>
+          <button className="bg-[#bfdf32] w-full cursor-pointer rounded-[0.5rem] h-[3rem] text-center font-bold">
+            Sign Up
+          </button>
         </div>
-       
-      <div className="bg-[#4abebd] w-[20rem] h-[15.5rem] p-[2.5rem] text-[#ffffff] rounded-br-[0.5rem]">
-        <h1 className="font-bold mb-[1rem]">Why Us</h1>
 
-        <p className="text-[#aff5f5] text-[1rem]">
-          Tutorials by industry experts Peer &amp; expert code review Coding
-          exercises Access to our GitHub repos Community forum Flashcard decks
-          New videos every week
-        </p>
-      </div>
+        <div className="bg-[#4abebd] w-[20rem] h-[15.5rem] p-[2.5rem] text-[#ffffff] rounded-br-[0.5rem] rounded-bl-[0.5rem] sm:rounded-bl-[0rem]">
+          <h1 className="font-bold mb-[1rem] text-[1.1rem]">Why Us</h1>
+
+          <p className="text-[#aff5f5] text-[1rem]">
+            Tutorials by industry experts Peer &amp; expert code review Coding
+            exercises Access to our GitHub repos Community forum Flashcard decks
+            New videos every week
+          </p>
+        </div>
       </div>
     </section>
   );
